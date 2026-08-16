f98ef1f8ad0e04bebaeeefc613fb2ae3b4ef1ace
Leyton <leyton@>
2026-08-16 11:09:51 -0400

    feat(metal): use real colors but no complex frag shading yet

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -34,7 +34,7 @@ int Engine::MainLoop() {
     //scene.dumpLong();
 
     Controller c;
-    NaiveRasterizer r;
+    MetalRasterizer r;
     //r.SetAxisDebug(true);
     r.SetNff(&scene);
     c.InitializeView(scene.GetFrom(), scene.GetUp(), scene.GetAt()); // 0,0,0 at (not always ?)
