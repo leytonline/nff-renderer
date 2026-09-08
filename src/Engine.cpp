bec22b344242c4d59eeae53471d4742c16ef3b8f
Leyton <leytonl1@umbc.edu>
2026-09-07 20:02:59 -0400

    chore: back to rasterizer

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -36,7 +36,7 @@ int Engine::MainLoop() {
     //scene.dumpLong();
 
     Controller c;
-    NaiveRaytracer r;
+    NaiveRasterizer r;
     //r.SetAxisDebug(true);
     r.SetNff(&scene);
     c.InitializeView(scene.GetFrom(), scene.GetUp(), scene.GetAt()); // 0,0,0 at (not always ?)
